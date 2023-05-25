173fe457de1584928523ce51a7bd1eac4fc384cf
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-05-25 21:44:09 +0900

    feat : cm camera

--- Assets/Scripts/Monsters/MekaSquidWard/Head.cs
+++ Assets/Scripts/Monsters/MekaSquidWard/Head.cs
@@ -2,26 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadMeakSquidWrad : MonoBehaviour
+public class Head : MonoBehaviour
 {
-    [SerializeField] GameObject bubble;
-    [SerializeField] float bossHp;
-
-
-    private Collider2D collider;
-
-
-
-    private void Awake()
-    {
-        collider = GetComponent<Collider2D>();
-    }
-
-    private void Update()
-    {
-        if (false)// 피격당하면
-            bossHp -= 20;
-    }
-
-
+    
 }

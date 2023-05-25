84d7f0ed8c09e9c6c02ed9ad8738367e47c2898a
dMinsz <dsminzks@gmail.com>
2023-05-25 16:47:32 +0900

    feat: Feat Boss1 Scripts

--- Assets/Scripts/Scenes/Base/SceneDefine.cs
+++ Assets/Scripts/Scenes/Base/SceneDefine.cs
@@ -10,6 +10,7 @@ public class SceneDefine
         LoadingScene, // 로딩씬
         TitleScene, // 타이틀 씬 
         RobbyScene, // 로비씬
+        FlyDutchManScene, // 보스 1
         EndingScene, // 게임 엔딩 씬 
     }
 }
---
+++ Assets/Scripts/Scenes/FlyDutchManScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyDutchManScene : BaseScene
+{
+    protected override void Init()
+    {
+        base.Init();
+        SceneType = SceneDefine.Scene.FlyDutchManScene;
+    }
+
+
+    public override void Clear()
+    {
+    }
+}
